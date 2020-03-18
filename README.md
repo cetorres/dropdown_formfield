3fe07b6ef58a5b77237623f98abce22377e8578a
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2020-03-18 20:51:46 +0530

    Readme updated

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ A dropdown form field using a dropdown button inside a form field.
 ## Example
 
 ```dart
+import 'package:flutter/material.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 void main() => runApp(MyApp());
@@ -75,7 +76,108 @@ class _MyHomePageState extends State<MyHomePage> {
               Container(
                 padding: EdgeInsets.all(16),
                 child: DropDownFormField(
+                 inputDecoration: OutlinedDropDownDecoration(labelText:"Welcome to island"),
+                  hintText: 'Please choose one',
+                  value: _myActivity,
+                  onSaved: (value) {
+                    setState(() {
+                      _myActivity = value;
+                    });
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _myActivity = value;
+                    });
+                  },
+                  dataSource: [
+                    {
+                      "display": "Running",
+                      "value": "Running",
+                    },
+                    {
+                      "display": "Climbing",
+                      "value": "Climbing",
+                    },
+                    {
+                      "display": "Walking",
+                      "value": "Walking",
+                    },
+                    {
+                      "display": "Swimming",
+                      "value": "Swimming",
+                    },
+                    {
+                      "display": "Soccer Practice",
+                      "value": "Soccer Practice",
+                    },
+                    {
+                      "display": "Baseball Practice",
+                      "value": "Baseball Practice",
+                    },
+                    {
+                      "display": "Football Practice",
+                      "value": "Football Practice",
+                    },
+                  ],
+                  textField: 'display',
+                  valueField: 'value',
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: DropDownFormField(
                   titleText: 'My workout',
+                  hintText: 'Please choose one',
+                  value: _myActivity,
+                  onSaved: (value) {
+                    setState(() {
+                      _myActivity = value;
+                    });
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _myActivity = value;
+                    });
+                  },
+                  dataSource: [
+                    {
+                      "display": "Running",
+                      "value": "Running",
+                    },
+                    {
+                      "display": "Climbing",
+                      "value": "Climbing",
+                    },
+                    {
+                      "display": "Walking",
+                      "value": "Walking",
+                    },
+                    {
+                      "display": "Swimming",
+                      "value": "Swimming",
+                    },
+                    {
+                      "display": "Soccer Practice",
+                      "value": "Soccer Practice",
+                    },
+                    {
+                      "display": "Baseball Practice",
+                      "value": "Baseball Practice",
+                    },
+                    {
+                      "display": "Football Practice",
+                      "value": "Football Practice",
+                    },
+                  ],
+                  textField: 'display',
+                  valueField: 'value',
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.all(16),
+                child: DropDownFormField(
+                  inputDecoration: RoundedDropDownDecoration(labelText:"Welcome to island"),
                   hintText: 'Please choose one',
                   value: _myActivity,
                   onSaved: (value) {
@@ -140,6 +242,8 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
 ```
 
 ## License
--- lib/dropdown_formfield.dart
+++ lib/dropdown_formfield.dart
@@ -2,6 +2,90 @@ library dropdown_formfield;
 
 import 'package:flutter/material.dart';
 
+class RoundedDropDownDecoration extends InputDecoration {
+
+  final double fontSize;
+  final String labelText;
+  final bool filled;
+  final double labelGap;
+  final Color borderColor;
+  final BorderRadius borderRadius;
+  final double borderWidth;
+
+  RoundedDropDownDecoration({
+    @required this.labelText,
+    this.fontSize,
+    this.filled,
+    this.labelGap,
+    this.borderColor,
+    this.borderRadius,
+    this.borderWidth})
+      :super(labelStyle: TextStyle(
+      fontSize: fontSize ?? 14,
+      textBaseline: TextBaseline.ideographic),
+      focusedBorder: OutlineInputBorder(
+          gapPadding: 1,
+          borderSide: BorderSide(
+              color: Colors.blue,
+              width: 3,
+              style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(60)),
+      border: OutlineInputBorder(
+          gapPadding: labelGap ??0,
+          borderSide: BorderSide(
+              color: borderColor ?? Colors.black,
+              width: borderWidth ?? 1,
+              style: BorderStyle.solid),
+          borderRadius: borderRadius ?? BorderRadius.circular(25)),
+      contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
+      labelText: labelText ?? "Title",
+      filled: filled ?? false);
+
+}
+
+class OutlinedDropDownDecoration extends InputDecoration {
+
+  final double fontSize;
+  final String labelText;
+  final bool filled;
+  final double labelGap;
+  final Color borderColor;
+  final BorderRadius borderRadius;
+  final double borderWidth;
+
+  OutlinedDropDownDecoration({
+    @required this.labelText,
+    this.fontSize,
+    this.filled,
+    this.labelGap,
+    this.borderColor,
+    this.borderRadius,
+    this.borderWidth})
+      :super(labelStyle: TextStyle(
+      fontSize: fontSize ?? 14,
+      textBaseline: TextBaseline.ideographic),
+      focusedBorder: OutlineInputBorder(
+          gapPadding: 2,
+          borderSide: BorderSide(
+              color: Colors.blue,
+              width: 3,
+              style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(0)),
+      border: OutlineInputBorder(
+          gapPadding: labelGap ?? 5,
+          borderSide: BorderSide(
+              color: borderColor ?? Colors.black,
+              width: borderWidth ?? 1,
+              style: BorderStyle.solid),
+          borderRadius: borderRadius ?? BorderRadius.circular(0)),
+      contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
+      labelText: labelText ?? "Title",
+      filled: filled ?? false);
+
+}
+
+
+
 class DropDownFormField extends FormField<dynamic> {
   final String titleText;
   final String hintText;
@@ -13,129 +97,67 @@ class DropDownFormField extends FormField<dynamic> {
   final String valueField;
   final Function onChanged;
   final bool filled;
-  final bool outlined;
-  final bool rounded;
-  final InputDecoration inputDecoration;
+  InputDecoration inputDecoration;
 
 
-  DropDownFormField(
-      {FormFieldSetter<dynamic> onSaved,
-      FormFieldValidator<dynamic> validator,
-      bool autovalidate = false,
-      this.titleText = 'Title',
-      this.hintText = 'Select one option',
-      this.required = false,
-      this.errorText = 'Please select one option',
-      this.value,
-      this.dataSource,
-      this.textField,
-      this.valueField,
-      this.onChanged,
-        this.filled = true,
-        this.inputDecoration,
-        this.outlined,
-        this.rounded
-      })
-      : super(
-          onSaved: onSaved,
-          validator: validator,
-          autovalidate: autovalidate,
-          initialValue: value == '' ? null : value,
-          builder: (FormFieldState<dynamic> state) {
-            if (outlined == null && rounded == null) {
-              inputDecoration = InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
-                labelText: titleText,
-                filled: filled,
-              );
-            } else {
-              assert(outlined ==
-                  rounded, "rounded and outlined must never be equal", );
-              if (rounded) {
-                inputDecoration = InputDecoration(
-                  labelStyle: TextStyle(
-                    fontSize: 14,
-                    textBaseline: TextBaseline.ideographic,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      gapPadding: 0,
-                      borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 1,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.circular(25)),
-                  border: OutlineInputBorder(
-                      gapPadding: 1,
-                      borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 1,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.circular(30)),
+  DropDownFormField({FormFieldSetter<dynamic> onSaved,
+    FormFieldValidator<dynamic> validator,
+    bool autovalidate = false,
+    this.titleText = 'Title',
+    this.hintText = 'Select one option',
+    this.required = false,
+    this.errorText = 'Please select one option',
+    this.value,
+    this.dataSource,
+    this.textField,
+    this.valueField,
+    this.onChanged,
+    this.filled = true,
+    this.inputDecoration,
+  }) :super(validator: validator,
+      onSaved: onSaved,
+      autovalidate: autovalidate,
+      initialValue: value == '' ? null : value,
+      builder: (FormFieldState<dynamic> state) {
+        return Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              InputDecorator(
+                decoration: inputDecoration??InputDecoration(
                   contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
                   labelText: titleText,
                   filled: filled,
-                );
-              } else if (outlined) {
-                inputDecoration = InputDecoration(
-                  labelStyle: TextStyle(
-                    fontSize: 14,
-                    textBaseline: TextBaseline.ideographic,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      gapPadding: 0,
-                      borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 1,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.circular(0)),
-                  border: OutlineInputBorder(
-                      gapPadding: 1,
-                      borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 1,
-                          style: BorderStyle.solid),
-                      borderRadius: BorderRadius.circular(0)),
-                  contentPadding: EdgeInsets.fromLTRB(12, 12, 8, 0),
-                  labelText: titleText,
-                  filled: filled,
-                );
-              }
-            }
-
-            return Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  InputDecorator(
-                    decoration: inputDecoration,
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<dynamic>(
-                        hint: Text(
-                          hintText,
-                          style: TextStyle(color: Colors.grey.shade500),
-                        ),
-                        value: value == '' ? null : value,
-                        onChanged: (dynamic newValue) {
-                          state.didChange(newValue);
-                          onChanged(newValue);
-                        },
-                        items: dataSource.map((item) {
-                          return DropdownMenuItem<dynamic>(
-                            value: item[valueField],
-                            child: Text(item[textField]),
-                          );
-                        }).toList(),
-                      ),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<dynamic>(
+                    hint: Text(
+                      hintText,
+                      style: TextStyle(color: Colors.grey.shade500),
                     ),
+                    value: value == '' ? null : value,
+                    onChanged: (dynamic newValue) {
+                      state.didChange(newValue);
+                      onChanged(newValue);
+                    },
+                    items: dataSource.map((item) {
+                      return DropdownMenuItem<dynamic>(
+                        value: item[valueField],
+                        child: Text(item[textField]),
+                      );
+                    }).toList(),
                   ),
-                  SizedBox(height: state.hasError ? 5.0 : 0.0),
-                  Text(
-                    state.hasError ? state.errorText : '',
-                    style: TextStyle(color: Colors.redAccent.shade700, fontSize: state.hasError ? 12.0 : 0.0),
-                  ),
-                ],
+                ),
               ),
-            );
-          },
+              SizedBox(height: state.hasError ? 5.0 : 0.0),
+              Text(
+                state.hasError ? state.errorText : '',
+                style: TextStyle(color: Colors.redAccent.shade700,
+                    fontSize: state.hasError ? 12.0 : 0.0),
+              ),
+            ],
+          ),
         );
+      });
+
 }
